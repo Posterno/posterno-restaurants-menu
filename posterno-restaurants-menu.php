697c66cf4b70dc10f271164148eb192a85b6f183
Alessandro Tesoro <alessandro.tesoro@icloud.com>
2020-02-06 11:56:23 +0000

    hello world

---
+++ posterno-restaurants-menu.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Plugin Name:     Posterno Restaurants Menu
+ * Plugin URI:      https://posterno.com/extensions/restaurants-menu
+ * Description:     Allows listings owners to create a restaurant menu for their listings.
+ * Author:          Posterno
+ * Author URI:      https://posterno.com
+ * Text Domain:     posterno-restaurants-menu
+ * Domain Path:     /languages
+ * Version:         1.0.0
+ *
+ * Posterno Restaurants Menu is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * any later version.
+ *
+ * Posterno Restaurants Menu is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Posterno Restaurants Menu. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @package posterno-restaurants-menu
+ * @author Sematico LTD
+ */
+
+defined( 'ABSPATH' ) || exit;
